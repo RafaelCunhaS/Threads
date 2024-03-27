28db72cdbfecd7fe7771cbf4fc7891f8e7727dec
Rafael Santos <rafaelcs_94@hotmail.com>
2024-03-27 20:24:53 -0300

    feat: update thread actions

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -55,3 +55,57 @@ export async function fetchThreads(pageNumber = 1, pageSize = 20) {
     throw new Error(`Error fetching threads: ${error.message}`)
   }
 }
+
+export async function fetchThreadById(id: string) {
+  try {
+    connectToDB()
+
+    const thread = await Thread.findById(id)
+      .populate({ path: 'author', model: User, select: '_id id name image' })
+      .populate({
+        path: 'children',
+        populate: [
+          { path: 'author', model: User, select: '_id id name parentId image' },
+          {
+            path: 'children',
+            model: Thread,
+            populate: { path: 'author', model: User, select: '_id id name parentId image' }
+          }
+        ]
+      })
+
+    return thread
+  } catch (error: any) {
+    throw new Error(`Error fetching thread by ID: ${error.message}`)
+  }
+}
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  try {
+    connectToDB()
+
+    const originalThread = await fetchThreadById(threadId)
+    if (!originalThread) {
+      throw new Error('Thread not found')
+    }
+
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId
+    })
+    const savedCommentThread = await commentThread.save()
+
+    originalThread.children.push(savedCommentThread._id)
+    await originalThread.save()
+
+    revalidatePath(path)
+  } catch (error: any) {
+    throw new Error(`Error adding comment to thread: ${error.message}`)
+  }
+}
