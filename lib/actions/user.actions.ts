7203f56cb3333e42490d2c2ef87cdbb16ef5d985
Rafael Santos <rafaelcs_94@hotmail.com>
2024-03-27 21:19:04 -0300

    feat: update user actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from 'next/cache'
 import User from '../models/user.model'
 import { connectToDB } from '../mongoose'
+import Thread from '../models/thread.model'
 
 interface Params {
   userId: string
@@ -44,5 +45,24 @@ export async function fetchUser(userId: string) {
     return await User.findOne({ id: userId.toString() })
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`)
+  }
+}
+
+export async function fetchUserThreads(userId: string) {
+  try {
+    connectToDB()
+
+    const threads = await User.findOne({ _id: userId.toString() }).populate({
+      path: 'threads',
+      model: Thread,
+      populate: {
+        path: 'children',
+        model: Thread,
+        populate: { path: 'author', model: User, select: 'name image id' }
+      }
+    })
+    return threads
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user threads: ${error.message}`)
   }
 }
